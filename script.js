1d6982d1bc1b0e3ba45365e2f7e311e4e70e2702
Julievogh <juvo0001@stud.kea.dk>
2023-09-10 18:58:34 +0200

    hjemmeside virker ok

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ window.addEventListener("resize", () => {
   }
 });
 
-
 // Hent alle faner og deres tilsvarende indholdselementer
 const tabs = document.querySelectorAll(".tab");
 const tabContents = document.querySelectorAll(".tab-content");
@@ -35,3 +34,27 @@ tabs.forEach((tab, index) => {
     }
   });
 });
+
+// script.js
+const boxes = document.querySelectorAll(".box");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+let currentIndex = 0;
+
+function showBox(index) {
+  boxes.forEach((box) => box.classList.remove("active"));
+  boxes[index].classList.add("active");
+}
+
+prevButton.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + boxes.length) % boxes.length;
+  showBox(currentIndex);
+});
+
+nextButton.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % boxes.length;
+  showBox(currentIndex);
+});
+
+// Show the first box by default on small screens
+showBox(currentIndex);
